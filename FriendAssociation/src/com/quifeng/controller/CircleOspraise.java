a214fafffce3845b123d4d711ef375ac3b33021e
jzh233 <1147981135@qq.com>
2020-12-27 21:07:16 +0800

    评论点赞、帖子分享、帖子点赞

---
+++ FriendAssociation/src/com/quifeng/controller/CircleOspraise.java
@@ -0,0 +1,27 @@
+package com.quifeng.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.quifeng.servlet.circle.ospraise.CircleOspraiseServlet;
+
+/**
+ * @desc   评论点赞
+ * @author JZH
+ * @time   2020-12-27
+ */
+@SuppressWarnings("serial")
+@WebServlet("/api/circle/ospraise")
+public class CircleOspraise extends HttpServlet{
+	
+	CircleOspraiseServlet osPraiseServlet = new CircleOspraiseServlet();
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		osPraiseServlet.AddOsZan(request,response);
+	}
+}
---
+++ FriendAssociation/src/com/quifeng/controller/CirclePostshare.java
@@ -0,0 +1,29 @@
+package com.quifeng.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.quifeng.servlet.circle.postshare.CirclePostshareServlet;
+
+/**
+ * @desc   分享帖子
+ * @author JZH
+ * @time   2020-12-27
+ */
+@SuppressWarnings("serial")
+@WebServlet("/api/circle/postshare")
+public class CirclePostshare extends HttpServlet{
+	
+	CirclePostshareServlet postShareServlet = new CirclePostshareServlet();
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		postShareServlet.SharePost(request,response);
+	}
+	
+}
